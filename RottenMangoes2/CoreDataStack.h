89544970cc0c6f6865bde14cd79ee69c792a444f
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-29 12:14:46 -0700

    added CoreData stack

---
+++ RottenMangoes2/CoreDataStack.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataStack.h
+//  Every.Do
+//
+//  Created by Jeremy Petter on 2015-05-26.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@end
